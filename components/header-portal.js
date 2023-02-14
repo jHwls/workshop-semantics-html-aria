8566b85d79658d82a3bd02b23c1c6c11d4500b0c
jhwls <info@jhowells.com>
2023-02-13 22:23:27 -0800

    Install prettier and format files

--- components/header-portal.js
+++ components/header-portal.js
@@ -1,16 +1,16 @@
-import React, { useEffect } from "react"
-import { createPortal } from "react-dom"
+import React, { useEffect } from "react";
+import { createPortal } from "react-dom";
 
-const HeaderPortal = ({children}) => {
-    const mount = document.getElementById("portal-root")
-    const el = document.createElement("div")
+const HeaderPortal = ({ children }) => {
+	const mount = document.getElementById("portal-root");
+	const el = document.createElement("div");
 
-    useEffect(() => {
-        mount.appendChild(el)
-        return () => mount.removeChild(el)
-    }, [el, mount])
-    
-    return createPortal(children, el)
-}
+	useEffect(() => {
+		mount.appendChild(el);
+		return () => mount.removeChild(el);
+	}, [el, mount]);
 
-export default HeaderPortal
+	return createPortal(children, el);
+};
+
+export default HeaderPortal;
--- components/utils/loaded-image-url.js
+++ components/utils/loaded-image-url.js
@@ -1,18 +1,18 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-const LoadedImageUrl = (imageMap = {}, imageSrc = '') => {
-    // imageMap glob returns images with extension as key ({png:''})
-    const imageObj = imageMap[imageSrc.split('.')[0]]
+const LoadedImageUrl = (imageMap = {}, imageSrc = "") => {
+	// imageMap glob returns images with extension as key ({png:''})
+	const imageObj = imageMap[imageSrc.split(".")[0]];
 
-    // Access child property no matter the file extension
-    const imageUrl = imageObj[Object.keys(imageObj)[0]]
+	// Access child property no matter the file extension
+	const imageUrl = imageObj[Object.keys(imageObj)[0]];
 
-    return imageUrl
-}
+	return imageUrl;
+};
 
 LoadedImageUrl.propTypes = {
-    imageMap: PropTypes.object.required,
-    imageSrc: PropTypes.string.required
-}
+	imageMap: PropTypes.object.required,
+	imageSrc: PropTypes.string.required
+};
 
-export default LoadedImageUrl
+export default LoadedImageUrl;
